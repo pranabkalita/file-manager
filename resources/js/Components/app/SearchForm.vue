54492f3e9b34bab19271ea2ecab288900a17db25
Pranab Kalita <pavankalita@gmail.com>
2023-08-29 21:25:01 +0530

    Implemented the authenticated styles

---
+++ resources/js/Components/app/SearchForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <form class="w-[600px] h-[80px] flex items-center">
+        <TextInput type="text" class="block w-full mr-2" v-model="form.search" autocomplete placeholder="Search for files and folder" />
+    </form>
+</template>
+
+<script setup>
+// Imports
+import TextInput from '@/Components/TextInput.vue';
+import { useForm } from '@inertiajs/vue3';
+
+// Uses
+const form = useForm({
+    search: ''
+})
+</script>
