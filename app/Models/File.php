898cff534b3cbade647e71e95c8dcc3efe11d574
Pranab Kalita <pavankalita@gmail.com>
2023-08-31 10:46:03 +0530

    Created the path of the folders and stored in the database.

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,18 +2,51 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
+use Kalnoy\Nestedset\NodeTrait;
 use App\Traits\HasCreatorAndUpdator;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Kalnoy\Nestedset\NodeTrait;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class File extends Model
 {
     use HasFactory, NodeTrait, SoftDeletes, HasCreatorAndUpdator;
 
+    // Hooks
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->parent) {
+                return;
+            }
+
+            $model->path = (!$model->parent->isRoot() ? $model->parent->path . '/' : '') . Str::slug($model->name);
+        });
+    }
+
+    // Methods
     public function isOwnedBy($userId): bool
     {
         return $this->created_by === $userId;
+    }
+
+    public function isRoot()
+    {
+        return $this->parent_id === null;
+    }
+
+    // Relations
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'parent_id');
     }
 }
